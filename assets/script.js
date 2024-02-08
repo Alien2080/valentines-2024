82cd4a2e5b409aca9f1ea9b7ad4c784c599674d9
Alien2080 <tom_aylen@hotmail.com>
2024-02-09 10:08:21 +1030

    add buttons and no functionality, new fonts

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,38 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const message2 = document.getElementById('message2');
 
   setTimeout(() => {
-    message.style.width = '270px';
-    message2.style.width = '400px';
+    message.style.width = '400px';
+    message2.style.width = '700px';
   }, 0);
  
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const noBtn = document.getElementById('noBtn');
+  const yesBtn = document.getElementById('yesBtn');
+  
+  noBtn.addEventListener('click', () => {
+    const heartContainer = document.querySelector('.heart-container');
+    
+    // Calculate max dimensions for movement
+    const maxX = heartContainer.offsetWidth - noBtn.offsetWidth;
+    const maxY = heartContainer.offsetHeight - noBtn.offsetHeight;
+    
+    // Generate random positions
+    const randomX = Math.random() * maxX;
+    const randomY = Math.random() * maxY;
+    
+    // Apply new positions
+    // noBtn.style.right = 'auto'; // Reset to use top and left instead
+    noBtn.style.bottom = `${randomY}px`;
+    noBtn.style.right = `${randomX}px`;
+  });
+
+  setTimeout(() => {
+    noBtn.style.opacity = '1';
+    yesBtn.style.opacity = '1';
+  }, 6000);
+
+
 });
 
