21a81636302667c50ae8307a04775bf412b39a32
Alien2080 <tom_aylen@hotmail.com>
2024-02-08 21:40:59 +1030

    add some content

---
+++ assets/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  const message = document.getElementById('message');
+  const message2 = document.getElementById('message2');
+
+  setTimeout(() => {
+    message.style.width = '270px';
+    message2.style.width = '400px';
+  }, 0);
+ 
+});
+
